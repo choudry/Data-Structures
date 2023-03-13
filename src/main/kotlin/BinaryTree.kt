b02ef342837021d212d0acb6a515cb13ddc03e14
Muhammad Usman <chmusman88@gmail.com>
2023-03-13 15:28:50 +0800

    Implement depth-first-traversal using loop and recursive function

--- src/main/kotlin/BinaryTree.kt
+++ src/main/kotlin/BinaryTree.kt
@@ -1,20 +1,76 @@
 
 data class Node (
-    var value: Int,
-    var left: Int,
-    var right: Int
-)
+    var value: Any
+) {
+    var left: Node? = null
+    var right: Node? = null
+}
 
-class Stack {
-    private val data: MutableList<Any> = mutableListOf()
+class Stack<T> {
+    private val data: MutableList<T> = mutableListOf()
 
-    fun push(input: Any) {
+    fun push(input: T) {
         data.add(0, input)
     }
 
-    fun pop(): Any = data.removeAt(0)
+    fun pop(): T = data.removeAt(0)
+
+    fun isEmpty() = data.isEmpty()
+}
+fun depthFirstValues(root: Node) {
+    /* Depth-First-Traversal
+    Time Complexity = O(n) each node is visited only once
+    Space Complexity = O(n)
+ */
+    val stack = Stack<Node>()
+    stack.push(root)
+    // List to add value of each node
+    val result: MutableList<Any> = mutableListOf()
+    while (!stack.isEmpty()) {
+        val current = stack.pop()
+        result.add(current.value)
+        current.right?.let { stack.push(it) }
+        current.left?.let { stack.push(it) }
+    }
+    result.forEach { println(it) }
 }
 
-class BinaryTree {
+fun recursiveDepthFirstTraversal(root: Node?): List<Any> {
+    root?.let {
+        val leftValues = recursiveDepthFirstTraversal(root.left)
+        val rightValues = recursiveDepthFirstTraversal(root.right)
+        return listOf(root.value) + leftValues + rightValues
+    }
+    return emptyList()
+}
 
+fun main() {
+    //Nodes declaration
+    /*
+        A
+      /    \
+     B      C
+   /   \      \
+  D     E      F
+ */
+
+    val a = Node('A')
+    val b = Node('B')
+    val c = Node('C')
+    val d = Node('D')
+    val e = Node('E')
+    val f = Node('F')
+    //Setting left and right's
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    depthFirstValues(a)
+
+    /*
+    Depth-First-Traversal using Recursion
+     */
+    val result = recursiveDepthFirstTraversal(a)
+    println(result)
 }
