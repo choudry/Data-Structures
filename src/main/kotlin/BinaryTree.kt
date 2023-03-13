d031941c9166b3abe47280fa6e5e42c7f82b207b
Muhammad Usman <chmusman88@gmail.com>
2023-03-13 10:57:18 +0800

    Created Stack class

--- src/main/kotlin/BinaryTree.kt
+++ src/main/kotlin/BinaryTree.kt
@@ -1,9 +1,20 @@
-class BinaryTree {
-
-}
 
 data class Node (
     var value: Int,
     var left: Int,
     var right: Int
 )
+
+class Stack {
+    private val data: MutableList<Any> = mutableListOf()
+
+    fun push(input: Any) {
+        data.add(0, input)
+    }
+
+    fun pop(): Any = data.removeAt(0)
+}
+
+class BinaryTree {
+
+}
