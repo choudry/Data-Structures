bb4ec43f4ed8e11559dbc02219f3a2ba17e7cdae
Muhammad Usman <chmusman88@gmail.com>
2023-03-31 11:31:29 +0800

    Add path traversal from undirected graph

--- src/main/kotlin/BinaryTree/BinaryTree.kt
+++ src/main/kotlin/BinaryTree/BinaryTree.kt
@@ -4,58 +4,6 @@ import Helper.Node
 import Helper.Queue
 import Helper.Stack
 
-fun depthFirstTraversal(root: Node): List<Any> {
-    /* Depth-First-Traversal
-    Time Complexity = O(n) each node is visited only once
-    Space Complexity = O(n)
- */
-    val stack = Stack<Node>()
-    stack.push(root)
-    // List to add value of each node
-    val result: MutableList<Any> = mutableListOf()
-    while (!stack.isEmpty()) {
-        val current = stack.pop()
-        result.add(current.value)
-        current.right?.let { stack.push(it) }
-        current.left?.let { stack.push(it) }
-    }
-
-    return result
-}
-
-fun recursiveDepthFirstTraversal(root: Node?): List<Any> {
-    root?.let {
-        val leftValues = recursiveDepthFirstTraversal(root.left)
-        val rightValues = recursiveDepthFirstTraversal(root.right)
-        return listOf(root.value) + leftValues + rightValues
-    }
-    return emptyList()
-}
-    //***********Breadth-First-Traversal*************
-/**
- * Breadth first traversal can be done using queue only, so there's no recursive approach
- * Time complexity = O(n) as every node is visited only once
- * Space complexity = O(n) as every node is saved only once
- */
-fun breadthFirstTraversal(root: Node?): List<Any> {
-        root?.let {
-            val queue = Queue<Node>()
-            val values: MutableList<Any> = mutableListOf()
-            queue.push(root)
-
-            while (queue.isNotEmpty()) {
-                val current = queue.shift()
-                values.add(current.value)
-
-                current.left?.let { queue.push(it) }
-                current.right?.let { queue.push(it) }
-            }
-
-            return values
-        }
-
-        return emptyList()
-}
 
 /**
     Tree include problem is to find out  either tree includes a specific value. We can find it with depth first search as well as breadth first
